e14de2c9cdfba9401f97e1d90db9d8504c6af4b3
Taylor Carpenter <taylor@codecafe.com>
2008-04-22 02:12:38 -0500

    lots of fun see http://pastie.caboo.se/pastes/107610/edit for another curl example from http://curl.haxx.se/lxr/source/docs/examples/simple.c see http://rubyinline.rubyforge.org/RubyInline/

---
+++ inline_c_example.rb
@@ -0,0 +1,41 @@
+require "inline"
+class MyTest
+	inline do |builder|
+		builder.include "<stdio.h>"
+		builder.include "<curl/curl.h>"
+		builder.include "<curl/easy.h>"
+		builder.add_compile_flags "-I/usr/include/curl"
+		builder.add_link_flags "-lcurl"
+
+		builder.c <<-"END"
+			long factorial(int max) {
+			int i=max, result=1;
+			while (i >= 2) { result *= i--; }
+			return result;
+		}
+		END
+
+		builder.c '
+			void hello(void) {
+			printf("hello\n");
+		}'
+
+		builder.c <<-"END"
+			int curltest(void) {
+				CURL *curl;
+				CURLcode res;
+				curl = curl_easy_init();
+				if(curl) {
+					curl_easy_setopt(curl, CURLOPT_URL, "curl.haxx.se");
+					res = curl_easy_perform(curl);
+					curl_easy_cleanup(curl);
+				}
+				return 0;
+			}
+		END
+	end
+end
+
+t = MyTest.new()
+t.hello
+t.curltest
