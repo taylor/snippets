d5c4d94479baa700cb548db4efc166fba61026c4
Taylor Carpenter <taylor@codecafe.com>
2009-02-03 21:18:43 -0600

    initial commit - unescaping stuff like \n to turn it into a literal newline

---
+++ unescape_literal.rb
@@ -0,0 +1,64 @@
+require 'pp'
+
+# Purpose: Unescape some characters from a string
+#
+# We are only dealing with the following charaters
+#
+#   \' – single quote
+#   \" – double quote
+#   \\ – single backslash
+#   \a – bell/alert
+#   \b – backspace
+#   \r – carriage return
+#   \n – newline
+#   \s – space
+#   \t – tab 
+#
+# Note:  In Vim just do
+#    :%s/\\n//g
+# or whatever the character is...
+
+def unescape(str)
+  str.gsub(/\\[\\'"abrnst]/) { |s|
+     case s[1].chr
+     when 'a'; "\a"
+     when 'b'; "\b"
+     when 'r'; "\r"
+     when 'n'; "\n"
+     when 's'; " "
+     when 't'; "\t"
+     when '\\'; "\\"
+     else
+       puts "unknown found char #{s[1].chr} ascii #{s[1]}"
+       s[1].chr
+     end 
+  }
+end
+
+s='newline\nbell\adoublequote\"backslash\\backspace\bcarriagereturn\rspace\stab\tEND'
+puts unescape(s)
+
+s='newline\nnewline\nnewline'
+puts s
+puts unescape(s)
+
+s='bells\abells\abells\a'
+puts s
+puts unescape(s)
+
+s='doublequotes\"doublequotes'
+puts s
+puts unescape(s)
+
+s="singlequote\\'singlequote\\'singlequote"
+puts s
+puts unescape(s)
+
+s='space\sspace\sspace'
+puts s
+puts unescape(s)
+
+s='backslash \\\\ backslash'
+puts s
+puts unescape(s)
+sn=unescape(s)
