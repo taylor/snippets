c2929b0239b76e43d3b3a52cef027a2cf684e579
Taylor Carpenter <taylor@codecafe.com>
2009-04-04 12:44:38 -0500

    added timeout for getting pages.  added option to choose random sites from link file (does not handle visiting the same sites)

--- threads/mech/thread_mech.rb
+++ threads/mech/thread_mech.rb
@@ -4,7 +4,7 @@ require 'date'
 require 'rubygems'
 require 'mechanize'
 require 'yaml'
-#require 'ruby-prof'
+require 'timeout'
 
 @threads = []
 
@@ -12,12 +12,19 @@ def threadit(url,n=nil)
   @threads << Thread.new {
     m = WWW::Mechanize.new
     m.user_agent_alias = 'Mac Safari'
+    title=''
 
     #p @threads
     x=n || "r-#{rand(100)}"
     puts "thread - #{x} STARTED - #{url}"
     begin
-      title = m.get(url).title.strip
+      Timeout::timeout(10) do
+        title = m.get(url).title.strip
+      end
+    rescue Timeout::Error
+      puts "Timed out trying to get url #{url}"
+    rescue SocketError
+      puts "Socket error for url #{url}...  maybe site now found?"
     rescue
       puts "exception in mech caught"
     end
@@ -31,19 +38,7 @@ totalthreads=ARGV[0] || 10
 totalthreads=totalthreads.to_i
 ARGV.shift
 
-# u = []
-# u << "http://www.google.com/"
-# u << "http://www.yahoo.com/"
-# u << "http://yellowpages.superpages.com/"
-# u << "http://www.linux.org/"
-# u << "http://netbsd.org/"
-# u << "http://digg.com/"
-# u << "http://msnbc.com/"
-# u << "http://www.wikipedia.org/"
-# u << "http://delicious.com/"
-# u << "http://www.facebook.com/"
-# u << "http://twitter.com/"
-
+@choose_rand = ARGV[0].nil? ? false : true
 
 if not File.exist?(linkfile)
   puts "Can't find #{linkfile}"
@@ -51,7 +46,6 @@ if not File.exist?(linkfile)
 end
 u = YAML.load_file(linkfile)
 
-# RubyProf.start
 
 start_t = Time.now
 start_dt = DateTime.parse(start_t.to_s)
@@ -78,29 +72,14 @@ totalthreads.times { |n|
     #n=rand(10)+1
     #sleep n
   end
-  threadit(u[n],n)
+  url = @choose_rand ? u.choice : u[n]
+  threadit(url,n)
 }
 
 
 @threads.compact!
-puts "what is in threads, #{@threads.inspect}, with size #{@threads.size}"
-r=0
-begin
-  @threads.each {|t| t.join}
-rescue NoMethodError
-  puts "no method error caught"
-end
+@threads.each {|t| t.join}
 
 end_t = Time.now
 puts "\nEnded #{DateTime.parse(end_t.to_s)}"
 puts "Total time in seconds - #{end_t-start_t}"
-
-# result = RubyProf.stop
-# 
-# Print a flat profile to text
-#printer = RubyProf::FlatPrinter.new(result)
-#printer.print(STDOUT, 0)
-#
-# Print a graph profile to text
-# printer = RubyProf::GraphPrinter.new(result)
-# printer.print(STDOUT, 0)
