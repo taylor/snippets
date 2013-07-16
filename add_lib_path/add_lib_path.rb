a735059ebc31228a641d9425724115c1b4ad480a
Taylor Carpenter <taylor@codecafe.com>
2013-07-15 23:24:53 -0500

    vendor lib stuff

--- add_lib_path/add_lib_path.rb
+++ add_lib_path/add_lib_path.rb
@@ -1,3 +1,7 @@
 script_path = Dir.chdir(File.expand_path(File.dirname(__FILE__))) { Dir.pwd }
 lib_path = Dir.chdir(script_path + '/../lib') { Dir.pwd }
 $:.unshift lib_path
+
+## To handle all vendor libs use:
+vendor_path = Dir.chdir(script_path + '/../vendor') { Dir.pwd }
+Dir["#{vendor_path}/*/lib"].map { |l| $:.unshift(l) if File.directory?(l) }
